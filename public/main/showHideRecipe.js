97913c8477cca174ecb72faf8c44450fcc082ea6
Cássia <cascrisbern@gmail.com>
2020-05-27 20:42:12 -0300

    refactoring the code

--- public/main/showHideRecipe.js
+++ public/main/showHideRecipe.js
@@ -30,20 +30,3 @@ for (showHide of showHides) {
 }
 
 
-
-/* ==== Links ==== */
-
-function linkActive(id, pathname) {
-  //console.log(`linkActive(id=${id}, pathname=${pathname}) was called`)
-  const link = document.querySelector(id)
-  //console.log(link)
-
-  if (document.location.pathname.includes(pathname)) {
-    //console.log(`if was executed for id ${id} and pathname ${pathname}`)
-    link.classList.add("active")
-  }
-}
-
-
-linkActive('#about', "/about")
-linkActive('#recipes', "/recipes")
